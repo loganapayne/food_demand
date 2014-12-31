fd49a6a1b0a239408129e4452993c3ef131cdca3
Tony Ta <tonyta.tt@gmail.com>
2014-12-30 16:13:32 -0800

    extract out functions

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
+var ew = function() {
+  window.location = "http://img1.wikia.nocookie.net/__cb20140924021657/walkingdead/images/5/51/Jimmy-fallon-ew.gif"
+}
+
+var searchYelp = function(searchTerm) {
+  window.location = "http://www.yelp.com/search?find_desc=" + searchTerm
+}
+
+var isYummy = function(food){
+  return food !== 'broccoli' && food !== 'swiss chard';
+}
+
 window.onload = function() {
   var buttons = document.getElementsByTagName("button");
-
   var firstButton = buttons[0];
   var secondButton = buttons[1];
 
   firstButton.onclick = function() {
-  
     var input = prompt("What Are You Hungry For?");
 
-    if (input !== "broccoli") {
-      window.location = "http://www.yelp.com/search?find_desc=" + input
+    if (isYummy(input)) {
+      searchYelp(input);
     } else {
-      window.location = "http://img1.wikia.nocookie.net/__cb20140924021657/walkingdead/images/5/51/Jimmy-fallon-ew.gif"
+      ew();
     }
-
   };
-
 
   secondButton.onclick = function() {
     prompt("What time do you want to eat?")
   };
 }
-
-
